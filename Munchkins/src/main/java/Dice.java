b702f0404d2a9de714c616efc02a6c2097a2909e
MarcoBatman <54103563+MarcoBatman@users.noreply.github.com>
2021-02-24 12:56:41 +0100

    jbh

--- Munchkins/src/main/java/Dice.java
+++ Munchkins/src/main/java/Dice.java
@@ -1,2 +1,13 @@
+import processing.core.PApplet;
+
 public class Dice {
+    PApplet p;
+    Dice(PApplet p){
+        this.p=p;
+    }
+int trowDie(int high){
+
+
+   return (int)p.random(0,high);
+}
 }
--- Munchkins/src/main/java/Players.java
+++ Munchkins/src/main/java/Players.java
@@ -1,2 +1,19 @@
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+
 public class Players {
+    PApplet p;
+    int level;
+    int gender;
+    int playerClass;
+    int race;
+    ArrayList<Card> hand = new ArrayList();
+    Players(PApplet p){
+        this.p=p;
+
+    }
+
+
+
 }
