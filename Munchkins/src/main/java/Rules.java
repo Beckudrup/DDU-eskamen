d337712c2adcdacdf7ae11632f04f428fbe3a11c
Beckudrup <de3gutter@gmail.com>
2021-04-09 15:37:21 +0200

    dh

--- Munchkins/src/main/java/Rules.java
+++ Munchkins/src/main/java/Rules.java
@@ -11,13 +11,13 @@ Rules(PApplet p){
 void displayRules(){
     p.textSize(32);
     p.text("Rules", p.width/2,50);
-    p.rect(20,120,240,850); // Turns
-    p.rect(290,120,240,850); // Start of game and death
-    p.rect(560,120,240,850); // Combat Run away ask for help
-    p.rect(830,120,240,850); // Class abilities
-    p.rect(1100,120,240,850); // Open the door Loot the room
-    p.rect(1370,120,240,850); // Curses
-    p.rect(1640,120,240,850); // How to win
+    p.rect(20,120,240,888); // Turns
+    p.rect(290,120,240,888); // Start of game and death
+    p.rect(560,120,240,888); // Combat Run away ask for help
+    p.rect(830,120,240,888); // Class abilities
+    p.rect(1100,120,240,888); // Open the door Loot the room
+    p.rect(1370,120,240,888); // Curses
+    p.rect(1640,120,240,888); // How to win
     p.fill(0);
     p.textSize(26);
 
@@ -29,8 +29,8 @@ void displayRules(){
     p.text("Death", 400,600);
 
     p.text("Combat", 670,160);
-    p.text("Run away", 670,570);
-    p.text("Ask for help", 670,770);
+    p.text("Run away", 670,550);
+    p.text("Ask for help", 670,810);
 
     p.text("Class abilities", 940,160);
 
@@ -42,19 +42,19 @@ void displayRules(){
     p.text("How to win", 1750,160);
 
     //Teksten til boksene
-    p.textSize(20);
-    p.textAlign(PConstants.LEFT);
-    p.text("When your turn starts\n you must Kick Open \nThe Door after that \ndepending on what you \ndraw different things \nwill happen. \nTo end your turn \npress the end turn \nbutton your cards \nwill then be hidden \nand the next player \ncan click start turn \nand then his cards \nwill be shown.", 30, 200);
     p.textSize(18);
-    p.text("At the end of your turn, \nif you have more than \n5 cards in hand you \nmust give cards untill \nyou are at 5 cards \nin hand to the player \nwith the lowest level, \nif you are the lowest \nlevel you discard them \ninstead.",30,730);
+    p.textAlign(PConstants.LEFT);
+    p.text("When your turn starts\nyou must Kick Open \nThe Door, after that \ndepending on what you \ndrew different things \nwill happen.\nExplained in \nOpen Door. \nTo end your turn \npress the end turn \nbutton, your cards \nwill then be hidden \nand the next player \ncan click start turn \nand then his cards \nwill be shown.", 30, 200);
+    p.textSize(18);
+    p.text("At the end of your turn, \nif you have more than \n5 cards in hand you \nmust give cards untill \nyou have 5 cards \nin hand, to the player \nwith the lowest level, \nif you are the lowest \nlevel you discard them \ninstead.",30,710);
     p.textSize(20);
-    p.text("The games starts out \nwith all players beging \nlevel 1 and drawing \ntwo room cards and \ntwo treassure cards, \nand then there is a \nround where you can \nplay your playable \ncards if you want \nbefore the game \nreally begins.",300,240);
-    p.text("If you lose a battle \nwhere the badstuff is \nyou die, then you have \nto start from the \nbegining at level 1 \nand draw your two \nroom cards and two \ntreassure cards.",300, 650);
+    p.text("The games starts out \nwith all players being \nlevel 1 and drawing \ntwo room cards and \ntwo treassure cards, \nand then there is a \nround where you can \nplay your playable \ncards if you want \nbefore the game \nreally begins.",300,240);
+    p.text("If you lose a battle \nwhere the badstuff is \nyou die, then you have \nto start from the \nbegining discarding all of\nyour items and go\ndown to level 1 \nand draw  two \nroom cards and two \ntreassure cards.",300, 650);
 
     p.textSize(16);
-    p.text("When you battle a \nmonster it will have \na level and to \nbeat it you must \nhave a higher power \nthan its level. \nYour power is your \nlevel and item \npower combined. \nIf you win you \nget treassure, if \nyou lose you get \nthe badstuff of that \nmonster.", 580,200);
-    p.text("If you can not \nbeat the monster\nthen you have the \nchance to run away.\nTo run away you \nmust roll 5 or more\nwith the dice.",580,590);
-    p.text("You also have the\nopportunity to call\nfor help, which \nlets another player \nhelp you, but \nyou must split the\ntreassure among you.",580,800);
+    p.text("When you battle a \nmonster it will have \na level and to \nbeat it you must \nhave a higher power \nthan its level. \nYour power is your \nlevel and item \npower combined. \nIf you win you \nget treassure and level, \nif you lose you get \nthe Badstuff of that \nmonster.", 580,185);
+    p.text("If you can not \nbeat the monster\nthen you have the \nchance to run away.\nTo run away you \nmust roll 5 or more\nwith the dice.\nIf you succed, then you \ndon't get the Badstuff",580,570);
+    p.text("You also have the\nopportunity to call\nfor help, which \nlets another player \nhelp you, but \nyou must split the\ntreassure among you.",580,830);
     p.textSize(20);
 
     p.text("Different classes have\ndifferent abilities, but\nyou activate them\nby clicking your class\nunder the right \nconditions.\nThe passive abilities\nare always activated.",840,200);
