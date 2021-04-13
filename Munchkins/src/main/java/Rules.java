2e46c2c10bac7a896b7e1b31629bd59752eb1d27
Beckudrup <de3gutter@gmail.com>
2021-04-13 15:30:27 +0200

    fix reglen

--- Munchkins/src/main/java/Rules.java
+++ Munchkins/src/main/java/Rules.java
@@ -49,17 +49,17 @@ void displayRules(){
     p.text("At the end of your turn, \nif you have more than \n5 cards in hand you \nmust give cards untill \nyou have 5 cards \nin hand, to the player \nwith the lowest level, \nif you are the lowest \nlevel you discard them \ninstead.",30,710);
     p.textSize(20);
     p.text("The games starts out \nwith all players being \nlevel 1 and drawing \ntwo room cards and \ntwo treassure cards, \nand then there is a \nround where you can \nplay your playable \ncards if you want \nbefore the game \nreally begins.",300,240);
-    p.text("If you lose a battle \nwhere the badstuff is \nyou die, then you have \nto start from the \nbegining discarding all of\nyour items and go\ndown to level 1 \nand draw  two \nroom cards and two \ntreassure cards.",300, 650);
+    p.text("If you lose a battle \nwhere the badstuff is \nyou die, then you have \nto start from the \nbegining discarding all\n of your items and go\ndown to level 1 \nand draw  two \nroom cards and two \ntreassure cards.",300, 650);
 
     p.textSize(16);
-    p.text("When you battle a \nmonster it will have \na level and to \nbeat it you must \nhave a higher power \nthan its level. \nYour power is your \nlevel and item \npower combined. \nIf you win you \nget treassure and level, \nif you lose you get \nthe Badstuff of that \nmonster.", 580,185);
-    p.text("If you can not \nbeat the monster\nthen you have the \nchance to run away.\nTo run away you \nmust roll 5 or more\nwith the dice.\nIf you succed, then you \ndon't get the Badstuff",580,570);
+    p.text("When you battle a \nmonster it will have \na level and to \nbeat it you must \nhave a higher power \nthan its level. \nYour power is your \nlevel and item \npower combined. \nIf you win you \nget treassure(s) and level(s), \nif you lose you get \nthe Badstuff of that \nmonster.", 580,185);
+    p.text("If you can not \nbeat the monster\nthen you have the \nchance to run away.\nTo run away you \nmust roll 5 or more\nwith the dice.\nIf you succed, then you \ndon't get the Badstuff.",580,570);
     p.text("You also have the\nopportunity to call\nfor help, which \nlets another player \nhelp you, but \nyou must split the\ntreassure among you.",580,830);
     p.textSize(20);
 
     p.text("Different classes have\ndifferent abilities, but\nyou activate them\nby clicking your class\nunder the right \nconditions.\nThe passive abilities\nare always activated.",840,200);
 
-    p.text("The first time\neach turn when you\nKick OpenThe Door\nthen the card you\ndrew is placed\nface-up and if it is\na class, race or spell\ncard you put it in\nyour hand, if it\nis a monster then\nyou battle it and\nif it is a curse\n it activates.",1120,200);
+    p.text("The first time\neach turn when you\nKick Open The Door\nthen the card you\ndrew is placed\nface-up and if it is\na class, race or spell\ncard you put it in\nyour hand, if it\nis a monster then\nyou battle it and\nif it is a curse\n it activates.",1120,200);
     p.text("If you drew a\ncard in Kick Open \nThe Door then you\ncan draw another\nRoom card face-down\nor play a monster\non yourself.",1120,690);
 
     p.text("If you have a\ncurse in your\nhand you can play \nit anytime, even\nif it is not\nyour turn.\nIf a curse is\nused in battle, \neven if it is\na curse that says\nnext battle -5 power\nit will count in\nthe present battle\nand be removed after.",1390,200);
