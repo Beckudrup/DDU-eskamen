bcaf9ca8dcf55b0e800b1ea168dceb28813d7ab2
Beckudrup <de3gutter@gmail.com>
2021-04-23 14:12:13 +0200

    fix små bogstaver

--- Munchkins/src/main/java/Main.java
+++ Munchkins/src/main/java/Main.java
@@ -56,15 +56,15 @@ public class Main extends PApplet {
         if (!loadingDone) {
             if (counter >= 0 && counter < 100) {
                 textSize(30);
-                text("loading.", width / 2, height / 2);
+                text("Loading.", width / 2, height / 2);
             }
             if (counter >= 100 && counter < 200) {
                 textSize(30);
-                text("loading..", width / 2, height / 2);
+                text("Loading..", width / 2, height / 2);
             }
             if (counter >= 200 && counter < 300) {
                 textSize(30);
-                text("loading...", width / 2, height / 2);
+                text("Loading...", width / 2, height / 2);
             }
             if (counter < 300) {
                 counter++;
