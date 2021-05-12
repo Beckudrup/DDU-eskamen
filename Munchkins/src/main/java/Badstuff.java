767918890932670a893a229ef159b77b943b62de
Marco Roland <69676695+iopoli0@users.noreply.github.com>
2021-05-12 13:54:22 +0200

    Yooo

--- Munchkins/src/main/java/Badstuff.java
+++ Munchkins/src/main/java/Badstuff.java
@@ -27,63 +27,65 @@ public class Badstuff {
     }
 
 
-    void badstufflevel(Players player, ArrayList<Players> playerList, int roll,Card monster) {
+    void badstufflevel(Players player, ArrayList<Players> playerList, int roll, Card monster) {
 
-        if(monster.badStuff.equalsIgnoreCase("- 1 level")){
-            player.level-= 1;
-            if (player.level<1){
-                player.level=1;
+        if (monster.badStuff.equalsIgnoreCase("- 1 level")) {
+            player.level -= 1;
+            if (player.level < 1) {
+                player.level = 1;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("- 2 level")){
-            player.level-= 2;
-            if (player.level<1){
-                player.level=1;
+        if (monster.badStuff.equalsIgnoreCase("- 2 level")) {
+            player.level -= 2;
+            if (player.level < 1) {
+                player.level = 1;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("- 3 level")){
-            player.level-= 3;
-            if (player.level<1){
-                player.level=1;
+        if (monster.badStuff.equalsIgnoreCase("- 3 level")) {
+            player.level -= 3;
+            if (player.level < 1) {
+                player.level = 1;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("- 2 level -3 if elf")){
-            if (!player.Class.name.equalsIgnoreCase("elf")){
-                player.level-= 2;}
-            if (player.Class.name.equalsIgnoreCase("elf")){
-                player.level-= 3;}
-            if (player.level<1){
-                player.level=1;
+        if (monster.badStuff.equalsIgnoreCase("- 2 level -3 if elf")) {
+            if (!player.Class.name.equalsIgnoreCase("elf")) {
+                player.level -= 2;
+            }
+            if (player.Class.name.equalsIgnoreCase("elf")) {
+                player.level -= 3;
+            }
+            if (player.level < 1) {
+                player.level = 1;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("- class if no -3lvl")){
-            player.level-= 3;
-            if (player.level<1){
-                player.level=1;
+        if (monster.badStuff.equalsIgnoreCase("- class if no -3lvl")) {
+            player.level -= 3;
+            if (player.level < 1) {
+                player.level = 1;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("- head and level - 1")){
-            player.level-= 1;
-            if (player.level<1){
-                player.level=1;
+        if (monster.badStuff.equalsIgnoreCase("- head and level - 1")) {
+            player.level -= 1;
+            if (player.level < 1) {
+                player.level = 1;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("set level = to lovest player")){
-            int lovestlvl=9;
+        if (monster.badStuff.equalsIgnoreCase("set level = to lovest player")) {
+            int lovestlvl = 9;
             for (int i = 0; i < playerList.size(); i++) {
-                if(playerList.get(i).level<lovestlvl){
-                    lovestlvl=playerList.get(i).level;
+                if (playerList.get(i).level < lovestlvl) {
+                    lovestlvl = playerList.get(i).level;
                 }
-                player.level=lovestlvl;
+                player.level = lovestlvl;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("roll die of less than 2 death else lose level = die")){
-            player.level-= roll;
-            if (player.level<1){
-                player.level=1;
+        if (monster.badStuff.equalsIgnoreCase("roll die of less than 2 death else lose level = die")) {
+            player.level -= roll;
+            if (player.level < 1) {
+                player.level = 1;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("player level = 1")){
+        if (monster.badStuff.equalsIgnoreCase("player level = 1")) {
             player.level = 1;
         }
     }
@@ -119,116 +121,114 @@ public class Badstuff {
     }
 
 
+    void badstuffloseitem(Card monster, Players player, ArrayList<Players> playerList, Dice die, Deck roomdisc, Deck treasuredisc) {
+        if (monster.badStuff.equalsIgnoreCase("all armor")) {
 
-
-    void badstuffloseitem(Card monster,Players player,ArrayList<Players> playerList,Dice die,Deck roomdisc,Deck treasuredisc) {
-        if(monster.badStuff.equalsIgnoreCase("all armor")){
-
-            if(player.body!=null){
+            if (player.body != null) {
                 treasuredisc.addcard(player.body);
-                player.body=null;
+                player.body = null;
             }
 
-            if(player.hand1!=null){
+            if (player.hand1 != null) {
                 treasuredisc.addcard(player.hand1);
-                player.hand1=null;
+                player.hand1 = null;
             }
 
-            if(player.hand2!=null){
+            if (player.hand2 != null) {
                 treasuredisc.addcard(player.hand2);
-                player.hand2=null;
+                player.hand2 = null;
             }
 
-            if(player.head!=null){
+            if (player.head != null) {
                 treasuredisc.addcard(player.head);
-                player.head=null;
+                player.head = null;
             }
 
-            if(player.feet!=null){
+            if (player.feet != null) {
                 treasuredisc.addcard(player.feet);
-                player.feet=null;
+                player.feet = null;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("foorgear")) {
+        if (monster.badStuff.equalsIgnoreCase("foorgear")) {
 
-            if(player.feet!=null){
+            if (player.feet != null) {
                 treasuredisc.addcard(player.feet);
-                player.feet=null;
+                player.feet = null;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("- class and race")) {
-            if(player.Class!=null){
+        if (monster.badStuff.equalsIgnoreCase("- class and race")) {
+            if (player.Class != null) {
                 roomdisc.addcard(player.Class);
-                player.Class=null;
+                player.Class = null;
             }
-            if(player.Class2!=null){
+            if (player.Class2 != null) {
                 roomdisc.addcard(player.Class2);
-                player.Class2=null;
+                player.Class2 = null;
             }
-            if(player.Race!=null){
+            if (player.Race != null) {
                 roomdisc.addcard(player.Race);
-                player.Race=null;
+                player.Race = null;
             }
-            if(player.Race2!=null){
+            if (player.Race2 != null) {
                 roomdisc.addcard(player.Race2);
-                player.Race2=null;
+                player.Race2 = null;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("- class if no -3lvl")){
-            if(player.Class!=null){
+        if (monster.badStuff.equalsIgnoreCase("- class if no -3lvl")) {
+            if (player.Class != null) {
                 roomdisc.addcard(player.Class);
-                player.Class=null;
+                player.Class = null;
             }
-            if(player.Class2!=null){
+            if (player.Class2 != null) {
                 roomdisc.addcard(player.Class2);
-                player.Class2=null;
+                player.Class2 = null;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("- headgear")) {
+        if (monster.badStuff.equalsIgnoreCase("- headgear")) {
 
-            if(player.head!=null){
+            if (player.head != null) {
                 treasuredisc.addcard(player.head);
-                player.head=null;
+                player.head = null;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("death if wizard instead lose class")){
-            if(player.Class!=null&&player.Class.name.equalsIgnoreCase("wizard")){
+        if (monster.badStuff.equalsIgnoreCase("death if wizard instead lose class")) {
+            if (player.Class != null && player.Class.name.equalsIgnoreCase("wizard")) {
                 roomdisc.addcard(player.Class);
-                player.Class=null;
+                player.Class = null;
             }
-            if(player.Class2!=null&&player.Class2.name.equalsIgnoreCase("wizard")){
+            if (player.Class2 != null && player.Class2.name.equalsIgnoreCase("wizard")) {
                 roomdisc.addcard(player.Class2);
-                player.Class2=null;
+                player.Class2 = null;
             }
         }
-        if(monster.badStuff.equalsIgnoreCase("lose all items and discard hand")){
-            if(player.body!=null){
+        if (monster.badStuff.equalsIgnoreCase("lose all items and discard hand")) {
+            if (player.body != null) {
                 treasuredisc.addcard(player.body);
-                player.body=null;
+                player.body = null;
             }
 
-            if(player.hand1!=null){
+            if (player.hand1 != null) {
                 treasuredisc.addcard(player.hand1);
-                player.hand1=null;
+                player.hand1 = null;
             }
 
-            if(player.hand2!=null){
+            if (player.hand2 != null) {
                 treasuredisc.addcard(player.hand2);
-                player.hand2=null;
+                player.hand2 = null;
             }
 
-            if(player.head!=null){
+            if (player.head != null) {
                 treasuredisc.addcard(player.head);
-                player.head=null;
+                player.head = null;
             }
 
-            if(player.feet!=null){
+            if (player.feet != null) {
                 treasuredisc.addcard(player.feet);
-                player.feet=null;
+                player.feet = null;
             }
-            if(player.utility!=null){
+            if (player.utility != null) {
                 treasuredisc.addcard(player.utility);
-                player.utility=null;
+                player.utility = null;
             }
         }
 
@@ -240,9 +240,6 @@ Mist specifikt gear piece
 
 
     }
-
-
-
 
 
 }
--- Munchkins/src/main/java/Players.java
+++ Munchkins/src/main/java/Players.java
@@ -342,46 +342,46 @@ public class Players {
             if (level <= 8) {
                 if (playable.name.equalsIgnoreCase("Hoard!")) {
                     //Draw three treasures
-                    p.println(1);
+                    PApplet.println(1);
                 }
                 if (playable.name.equalsIgnoreCase("Invoke obscure rules")) {
                     level = level + 1;
-                    p.println(1);
+                    PApplet.println(1);
                 }
                 if (playable.name.equalsIgnoreCase("Bribe GM with food")) {
-                    p.println(2);
+                    PApplet.println(2);
                     level = level + 1;
                 }
                 if (playable.name.equalsIgnoreCase("Potion of general studliness")) {
                     level = level + 1;
-                    p.println(3);
+                    PApplet.println(3);
                 }
                 if (playable.name.equalsIgnoreCase("1,000 gold peices")) {
                     level = level + 1;
-                    p.println(4);
+                    PApplet.println(4);
                 }
                 if (playable.name.equalsIgnoreCase("Boil an anthill")) {
-                    p.println(5);
+                    PApplet.println(5);
                     level = level + 1;
                 }
                 if (playable.name.equalsIgnoreCase("Convenient addition error")) {
                     level = level + 1;
-                    p.println(6);
+                    PApplet.println(6);
                 }
                 if (playable.name.equalsIgnoreCase("Mutilate the bodies")  /*&& backgroundSyste.battlefase ;; end of *any* combat */) {
                     level = level + 1;
-                    p.println(7);
+                    PApplet.println(7);
 
                 }
                 //If hireling is on the bord utility.name.equalsIgnoreCase("Hireling")
                 if (playable.name.equalsIgnoreCase("Kill the hireling")) {
                     level = level + 1;
-                    p.println(8);
+                    PApplet.println(8);
                 }
                 if (playable.name.equalsIgnoreCase("Steal a level")) {
                     //Selected person level = level -1;
                     level = level + 1;
-                    p.println(9);
+                    PApplet.println(9);
                 }
             }
             if (playable.name.equalsIgnoreCase("Wand of dowsing")) {
