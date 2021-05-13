8926ff443de75ecdf8009e0ad1377c8511bc0831
Beckudrup <de3gutter@gmail.com>
2021-05-13 20:54:55 +0200

    Revert "haiya"
    This reverts commit 060adcbfce06f9c7166763800a7fe899199f4fdb.

--- Munchkins/src/main/java/Button.java
+++ Munchkins/src/main/java/Button.java
@@ -44,7 +44,9 @@ public class Button {
         }
     }
 
-
+    void valgt() {
+        p.triangle(xPos + w / 2, yPos + h + 10, xPos + w / 2 - w / 4, yPos + h + 30, xPos + w / 2 + w / 4, yPos + h + 30);
+    }
 
     void release() {
         tryk = false;
